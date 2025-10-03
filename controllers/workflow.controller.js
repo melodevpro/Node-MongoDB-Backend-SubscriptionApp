4eac118318846df8e5cb53b7711f3c42a28e6a34
melomatthewdev <devscustomers@outlook.com>
2025-10-02 19:27:21 -0500

    config workflow

--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -1,6 +1,27 @@
+import dayjs from 'dayjs';
+import Subscription from '../models/Subscription.model.js';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
 const { serve } = require('@upstash/workflow/express');
 
-export const sendReminders = serve();
+export const sendReminders = serve( async(context) => {
+    const { subscriptionId } = context.requestPayload;
+    const subscription = await fetchSubscription(context, subscriptionId);
+});
 
-export const getReminders = serve();
+if (!subscription || subscription.status !== 'active') return;
+
+const renewalDate = dayjs(subscription.renewalDate);
+const today = dayjs();
+
+if(renewalDate.isBefore(dayjs())) {
+    console.log(`Enviando recordatorio de renovacion de ${subscriptionId}`);
+    return;
+}
+
+const fetchSubscription = async (context, subscriptionId) => {
+    return await context.run('get subscription', () => {
+        return Subscription.findById(subscriptionId).populate('user', 'name email');
+    });
+};
